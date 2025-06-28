b195cf93a3c2308b020240af6749e220a431254c
jy-ptl <jyptl887@gmail.com>
2025-06-28 06:38:41 +0000

    enable JPA auditing.

---
+++ user-service/src/main/java/com/voyagrr/userservice/config/Auditable.java
@@ -0,0 +1,23 @@
+package com.voyagrr.userservice.config;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdOn;
+
+    @LastModifiedDate
+    private Instant updatedOn;
+
+}
