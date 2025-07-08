fda25b5800635da4125dde068e0d492fd4fab63c
jy-ptl <jyptl887@gmail.com>
2025-07-08 06:29:16 +0000

    add encoding-service

---
+++ encoding-service/src/test/java/com/voyagrr/encodingservice/EncodingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voyagrr.encodingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncodingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
